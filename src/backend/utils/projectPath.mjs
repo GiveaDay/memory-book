5fe7ad867c17ef823deb5a6b29dadbb66ee172b8
Koen Deschacht <koendeschacht@gmail.com>
2022-11-07 11:15:58 +0100

    Fixed getting the project path on windows, fixed license in package.json

--- src/backend/utils/projectPath.mjs
+++ src/backend/utils/projectPath.mjs
@@ -1,6 +1,7 @@
 import path from "path";
+import {fileURLToPath} from 'url'
 
 export function getProjectPath(pathRelativeToProjectRoot) {
-    let moduleURL = new URL(import.meta.url)
-    return path.join(path.join(path.dirname(moduleURL.pathname), '../../../'), pathRelativeToProjectRoot)
+    let modulePath = fileURLToPath(import.meta.url)
+    return path.join(path.join(path.dirname(modulePath), '../../../'), pathRelativeToProjectRoot)
 }
